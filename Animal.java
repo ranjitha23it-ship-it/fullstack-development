192de371c2a391ca987f7ec7e19c803a114b6c5b
ranjitha23it-ship-it <ranjitha.23it@npsbcet.edu.in>
2025-08-29 15:36:49 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.println(name + " eats food.");
+    }
+
+    public void sleep() {
+        System.out.println(name + " sleeps peacefully.");
+    }
+}
